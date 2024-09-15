1fce75bb6811ec09ccae440d9b92472dfbbb8029
Roman <melnychukroman22@gmail.com>
2024-09-15 16:02:59 +0300

    Move validation script to external JS file

---
+++ static/js/validateForm.js
@@ -0,0 +1,12 @@
+function validateForm() {
+    var title = document.forms[0]["title"].value;
+    var tag = document.forms[0]["tag"].value;
+    var description = document.forms[0]["description"].value;
+    var dueDate = document.forms[0]["due_date"].value;
+
+    if (title == "" || tag == "" || description == "" || dueDate == "") {
+        alert("Всі поля повинні бути заповнені");
+        return false;
+    }
+    return true;
+}
